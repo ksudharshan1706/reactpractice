bba6fe94b4fcb4e51508acb8a26705aa8a6a6551
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2022-12-10 14:27:33 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function Welcome() {
       Rating: "8.0",
       Moviedesc:
         "A fearless revolutionary and an officer in the British force, who once shared a deep bond, decide to join forces and chart out an inspirational path of freedom against the despotic rulers.",
+      trailer: "https://www.youtube.com/watch?v=NgBoMJy386M",
     },
     {
       movieDp:
@@ -22,6 +23,7 @@ function Welcome() {
       Rating: "8.2",
       Moviedesc:
         "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+      trailer: "https://www.youtube.com/watch?v=sOEg_YZQsTI",
     },
     {
       movieDp:
@@ -30,8 +32,8 @@ function Welcome() {
       Rating: "6.9",
       Moviedesc:
         "HIT 2 is an upcoming Indian Telugu-language action thriller film written and directed by Sailesh Kolanu. The second installment in The HIT Verse, the film stars Adivi Sesh and Meenakshi",
+      trailer: "https://www.youtube.com/watch?v=-OMTthapaWE",
     },
-
     {
       movieDp:
         "https://assets-prd.ignimgs.com/2022/05/25/starwarssaga-blogroll-1653501853399.jpg",
@@ -39,18 +41,20 @@ function Welcome() {
       Rating: "8.7",
       Moviedesc:
         "Star Wars is an American epic space opera multimedia franchise created by George Lucas, which began with the eponymous 1977 film and quickly became a worldwide pop-culture phenomenon.",
+      trailer: "https://www.youtube.com/watch?v=8Qn_spdM5Zg",
     },
   ]);
-
   return (
-    <section className="pricing py-5  mainBack">
-      <button type="button" class="btn btn-light" onClick={on}>
-        + Add Movie
+    <section className="pricing py-5 mainBack">
+      <div className="movieName">
+        <p>MOVIES</p>
+      </div>
+      <button type="button" className="btn btn-light btnFloat" onClick={on}>
+        <i class="fa-solid fa-plus"></i>
       </button>
+      <br></br>
       <div id="formcontainer1" className="container formcontainer">
-        {/* <div className="closeBox"> */}
         <i class="fa-solid fa-xmark" onClick={off}></i>
-        {/* </div> */}
         <form className="formdata">
           <input
             className="form-control"
@@ -80,6 +84,13 @@ function Welcome() {
             placeholder="Movie Desc"
           ></input>
           <br></br>
+          <input
+            className="form-control"
+            type="text"
+            id="trailer"
+            placeholder="Add trailer"
+          ></input>
+          <br></br>
           <button
             className="btnclass btn btn-primary"
             onClick={(e) => {
@@ -99,7 +110,7 @@ function Welcome() {
         </form>
       </div>
       {console.log("here newmovie", newMovie)}
-      <div className="container">
+      <div className="container" onClick={off}>
         <div className="row">
           {pricecart.map((user) => (
             <div className="col-lg-3 mt-3">
@@ -108,12 +119,18 @@ function Welcome() {
                   <img className="img-pic" src={user.movieDp}></img>
                   <b>
                     <div className="MovieName">
-                      <div>{user.MovieName}</div>
+                      <div>
+                        {user.MovieName}{" "}
+                        <a href={user.trailer} target="blank">
+                          <i class="fa-brands fa-youtube"></i>
+                        </a>
+                      </div>
                       <div>⭐ {user.Rating}</div>
                     </div>
                   </b>
                   <hr></hr>
                   <p>{user.Moviedesc}</p>
+                  <Counter />
                 </div>
               </div>
             </div>
@@ -131,14 +148,31 @@ function on() {
   formEle.style.display = "block";
 }
 
-// function on() {
-//   document.getElementById("overlay").style.display = "block";
-// }
-
 function off() {
   var formEle = document.getElementById("formcontainer1");
   console.log(formEle);
   formEle.style.display = "none";
+}
+
+function Counter() {
+  let [like, setLike] = useState(0);
+  let [dislike, setDisLike] = useState(0);
+
+  const messageStyle = {
+    display: like - dislike >= 10 ? "block" : "none",
+  };
+  return (
+    <div className="likeDiv">
+      {/* <h2 style={messageStyle}>You are Awesome</h2> */}
+      {like - dislike >= 10 ? <h6>Must Watch 😍</h6> : null}
+      <button className="likeBtn" onClick={() => setLike(like + 1)}>
+        😍{like}
+      </button>
+      <button className="likeBtn" onClick={() => setDisLike(dislike + 1)}>
+        😭{dislike}
+      </button>
+    </div>
+  );
 }
 
 export default App;
